8bb01d3e80f57867a8b23c8e59d1d54e4e2cc62c
luddetgittar2 <55787807+luddetgittar2@users.noreply.github.com>
2019-10-17 13:51:25 +0200

    gjorde klart designen

--- kalkylator/src/sample/Main.java
+++ kalkylator/src/sample/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         BorderPane pane = new BorderPane();
 
         TextField text = new TextField();
@@ -20,32 +21,69 @@ public class Main extends Application {
 
 
 
-
-        Button button1 = new Button();
-        Button button2= new Button();
-        Button button3 = new Button();
-        Button button4 = new Button();
-        Button button5 = new Button();
-        Button button6 = new Button();
-        Button button7 = new Button();
-        Button button8 = new Button();
-        Button button9 = new Button();
+        Button button1 = new Button("1");
+        button1.setMinSize(125, 100);
+        Button button2= new Button("2");
+        button2.setMinSize(125, 100);
+        Button button3 = new Button("3");
+        button3.setMinSize(125, 100);
+        Button button4 = new Button("4");
+        button4.setMinSize(125, 100);
+        Button button5 = new Button("5");
+        button5.setMinSize(125, 100);
+        Button button6 = new Button("6");
+        button6.setMinSize(125, 100);
+        Button button7 = new Button("7");
+        button7.setMinSize(125, 100);
+        Button button8 = new Button("8");
+        button8.setMinSize(125, 100);
+        Button button9 = new Button("9");
+        button9.setMinSize(125, 100);
+        Button button0 = new Button("0");
+        button0.setMinSize(125, 100);
+        Button buttonPlus = new Button("+");
+        buttonPlus.setMinSize(125, 100);
+        Button buttonMin = new Button("-");
+        buttonMin.setMinSize(125, 100);
+        Button buttonTimes = new Button("X");
+        buttonTimes.setMinSize(125, 100);
+        Button buttonDivide = new Button("/");
+        buttonDivide.setMinSize(125, 100);
+        Button buttonClear = new Button("Clear");
+        buttonClear.setMinSize(500, 100);
+        Button buttonDot = new Button(".");
+        buttonDot.setMinSize(125, 100);
+        Button buttonEct = new Button("=");
+        buttonEct.setMinSize(125, 100);
 
         GridPane grid = new GridPane();
-        grid.add(button1, 0,1);
-        grid.add(button2, 1,1);
-        grid.add(button3, 2,1);
+
+        grid.add(button1, 0,3);
+        grid.add(button2, 1,3);
+        grid.add(button3, 2,3);
         grid.add(button4, 0,2);
         grid.add(button5, 1,2);
         grid.add(button6, 2,2);
-        grid.add(button7, 0,3);
-        grid.add(button8, 1,3);
-        grid.add(button9, 2,3);
+        grid.add(button7, 0,1);
+        grid.add(button8, 1,1);
+        grid.add(button9, 2,1);
+        grid.add(button0, 1,4);
+        grid.add(buttonPlus, 0,4);
+        grid.add(buttonMin, 2,4);
+        grid.add(buttonDivide, 3,1);
+        grid.add(buttonTimes, 3,2);
+        grid.add(buttonDot, 3,3);
+        grid.add(buttonEct, 3,4);
 
         pane.setCenter(grid);
+        pane.setBottom(buttonClear);
+
+        button1.setOnAction();
+
 
         primaryStage.setTitle("Kalkylator");
-        primaryStage.setScene(new Scene(pane, 500, 700));
+        primaryStage.setScene(new Scene(pane, 490, 500));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
